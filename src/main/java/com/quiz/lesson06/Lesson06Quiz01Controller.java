1ac146576409f13346ec0966d4285ed802ead425
DESKTOP-HME3UEB\403megait <test@test.com>
2024-12-24 18:45:03 +0900

    과제1 일부

---
+++ src/main/java/com/quiz/lesson06/Lesson06Quiz01Controller.java
@@ -0,0 +1,46 @@
+package com.quiz.lesson06;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/lesson06/quiz01")
+@Controller
+public class Lesson06Quiz01Controller {
+
+	// 즐겨찾기 추가 화면
+	@GetMapping("/add-bookmark-view")
+	public String addBookmarkView() {
+		return "lesson06/addBookmark";
+	}
+	
+	// AJAX 요청
+	// 즐겨찾기 추가
+	@ResponseBody
+	@PostMapping("/add-bookmark")
+	public Map<String, Object> addBookmark(
+			@RequestParam("name") String name,
+			@RequestParam("url") String url) {
+		
+		// db insert
+		
+		// 성공 응답
+		// {"code":200, "result":"성공"}
+		// {"code":500, "error_message":"에러 원인"}
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("result", "성공");
+		return result; // json string 응답
+	}
+	
+	// 즐겨찾기 목록 화면
+}
+
+
+
