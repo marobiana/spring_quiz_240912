e83a2be7b6633dc23223f0c04a6ca35fb850b6f2
DESKTOP-HME3UEB\403megait <test@test.com>
2024-12-27 18:45:13 +0900

    1번 뿌리기만

--- src/main/java/com/quiz/booking/BookingController.java
+++ src/main/java/com/quiz/booking/BookingController.java
@@ -1,16 +1,32 @@
 package com.quiz.booking;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quiz.booking.bo.BookingBO;
+import com.quiz.booking.domain.Booking;
 
 @RequestMapping("/booking")
 @Controller
 public class BookingController {
+	
+	@Autowired
+	private BookingBO bookingBO;
 
 	// 예약 목록 화면
 	@GetMapping("/booking-list-view")
-	public String bookingListView() {
+	public String bookingListView(Model model) {
+		// db select
+		List<Booking> bookingList = bookingBO.getBookingList();
+		
+		// model
+		model.addAttribute("bookingList", bookingList);
+		
 		return "booking/bookingList";
 	}
 	
